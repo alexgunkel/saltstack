4cd18e584e36ff46009f707eb943cfafdd3b6f6e
Alexander Gunkel <alexander.gunkel@netresearch.de>
2017-09-12 14:11:55 +0200

    Add phpdepend

--- resources/00-aliases.sh
+++ resources/00-aliases.sh
@@ -1,2 +1,3 @@
 alias pw="pwgen -cny 32 1"
 alias rebase="git fetch --all && git pull && git rebase -i origin/master"
+alias phpdepend='pdepend --summary-xml=./sum.xml --jdepend-chart=./jdepend.svg --overview-pyramid=./pyramid.svg .'
