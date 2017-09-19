c3cc3a5b4ff182d7f5b6482cff3d181a3976f846
Alexander Gunkel <alexander.gunkel@netresearch.de>
2017-09-19 13:18:03 +0200

    Add cd-alternatives

--- resources/00-aliases.sh
+++ resources/00-aliases.sh
@@ -1,3 +1,7 @@
 alias pw="pwgen -cny 32 1"
+alias pull="git fetch --all && git pull"
 alias rebase="git fetch --all && git pull && git rebase -i origin/master"
 alias phpdepend='pdepend --summary-xml=./sum.xml --jdepend-chart=./jdepend.svg --overview-pyramid=./pyramid.svg .'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
